36ec3c36966ae8977a8cd343315e7ee714a1d971
Matthew Clarke <m.d.clarke.sa@gmail.com>
2026-01-21 14:25:53 +0200

    small changes to Gantt

--- frontend/calendar-vanilla/vite.config.ts
+++ frontend/calendar-vanilla/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    port: 5173
-  },
-  optimizeDeps: {
-    include: ['@bryntum/calendar', '@bryntum/calendar-react']
-  }
-})
+    server : {
+        port : 5173
+    },
+    optimizeDeps : {
+        include : ['@bryntum/calendar', '@bryntum/calendar-react']
+    }
+});
